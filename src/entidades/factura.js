151758aea11deb16e97379b7252a44ae9d00350b
JuaniRodriguez <106626992+JuaniRodriguez@users.noreply.github.com>
2023-04-03 09:16:57 -0300

    Update factura.js

--- src/entidades/factura.js
+++ src/entidades/factura.js
@@ -1,9 +1,10 @@
 class Factura {
-    constructor(id,fecha,cliente,vencimiento) {
+    constructor(id,fecha,cliente,vencimiento,servicio) {
         this.id=id
         this.fecha=fecha
         this.cliente=cliente
         this.vencimiento=vencimiento
+        this.servicio=servicio
     }
 
     aplicarDescuento()
